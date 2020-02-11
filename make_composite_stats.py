fdca6339d001086b0bb7e8978b4c17a66af5c794
smithB <bsmith@apl.washington.edu>
2020-02-10 21:50:38 -0800

    make_composite_stats.py: work on resolution of results

--- make_composite_stats.py
+++ make_composite_stats.py
@@ -15,31 +15,34 @@ from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 import scipy.integrate as sciint
 
+
+def interp_inv(fi, t0, f0):
+    ii_plus =np.min(np.flatnonzero(fi>=f0))
+    ii_minus=np.max(np.flatnonzero(fi<=f0))
+    if f0[ii_plus]==f0[ii_minus]:
+        return (t0[ii_plus]+t0[ii_minus])/2
+    return t0[ii_minus]+(fi-f0[ii_minus])*(t0[ii_plus]-t0[ii_minus])/(f0[ii_plus]-f0[ii_minus])
+
 def TX_corr(TX, T_win, sigma, TX_sigma=None):
     if TX_sigma is None:
         TX_sigma=TX.robust_spread()
     sigma_extra=np.sqrt(np.max([0, sigma**2-TX_sigma**2]))
     TXb=waveform(TX.t, TX.p.copy()).broaden(sigma_extra)
-    IPT=sciint.cumtrapz((TXb.p*TXb.t).ravel(), TXb.t.ravel(), initial=0)
-    IP=sciint.cumtrapz(TXb.p.ravel(), TXb.t.ravel(), initial=0)
-    C=lambda tt:np.diff(np.interp(tt+np.array([-T_win/2,T_win/2]), TXb.t.ravel(), IPT))/np.diff(np.interp(tt+np.array([-T_win/2,T_win/2]), TXb.t.ravel(), IP))
-    tc=C(0)
-    tc_last=tc-10
-    while np.abs(tc-tc_last) >0.01:
-        tc_last=tc
-        tc=C(tc)
+    return wf_med_bar(TXb, T_win, t_tol=0.01*0.25)
+
+def wf_med_bar(WF, T_win, t_tol=0.01):
+    IPT=sciint.cumtrapz((WF.p*WF.t).ravel(), WF.t.ravel(), initial=0)
+    IP=sciint.cumtrapz(WF.p.ravel(), WF.t.ravel(), initial=0)
+    C=lambda tt, W:np.diff(np.interp(tt+np.array([-W/2,W/2]), WF.t.ravel(), IPT))/np.diff(np.interp(tt+np.array([-W/2,W/2]), WF.t.ravel(), IP))
+    tc=C(0, T_win)
+    tc_last=tc-10*t_tol
+    while np.abs(tc-tc_last) > t_tol:
+        tc_last=tc.copy()
+        tc=C(tc, T_win)
     # find the integrand of IP at Tc-T_win/2, Tc+T_win/2
-    IPW=np.interp(np.array([tc-T_win/2, tc+T_win/2]), TXb.t.ravel(), IP)
+    IPW=np.interp(np.array([tc-T_win/2, tc+T_win/2]), WF.t.ravel(), IP)
     # find the integrand value at the median
-    IPWc=np.mean(IPW)
-    # find the first index  of Ip above IPWc
-    imed_plus=np.min(np.flatnonzero(IP > IPWc))
-    imed_minus=np.max(np.flatnonzero(IP < IPWc))
-    if IP[imed_plus]==IP[imed_minus]:
-        t_med=(TXb.t[imed_plus]+TXb.t[imed_minus])/2.
-    else:
-        t_med=TXb.t[imed_minus]+(TXb.t[imed_plus]-TXb.t[imed_minus])/(IPT[imed_plus]-IPT(imed_minus))
-    
+    t_med=interp_inv(np.mean(IPW), WF.t, IP)
     return tc, t_med
 
 def make_composite_wf(k0s, catalog):
@@ -67,8 +70,7 @@ def composite_stats(WF, t_window):
     sigma_r=WF.robust_spread(els=els)
     sigma=WF.sigma(els=els)
     med=WF.percentile(0.5, els=els)
-    return ctr, med, sigma, sigma_r    
-
+    return ctr, med, sigma, sigma_r
 
 def my_lsfit(G, d):
     try:
@@ -90,12 +92,12 @@ def plane_fit_R(D, ind):
     G=np.c_[np.ones(ind), D.x[ind].ravel(), D.y[ind].ravel()]
     m, R, sigma_hat = my_lsfit(G, D.elevation)
     return R, np.sqrt(np.sum(m[1:2]))
-    
+
 def setup(scat_file, impulse_file):
-   
+
     with h5py.File(impulse_file,'r') as h5f:
         TX=waveform(np.array(h5f['/TX/t']),np.array(h5f['/TX/p']))
-    
+
     TX.t *= 1e9
     TX.t -= TX.nSigmaMean()[0]
     TX.tc = 0
@@ -107,14 +109,14 @@ def setup(scat_file, impulse_file):
     TX.normalize()
 
     # make the library of templates
-   
+
     catalog = dict()
     catalog.update({0.:TX})
     catalog.update(make_rx_scat_catalog(TX, h5_file=scat_file))
     return catalog
 
 def make_composite_stats(k0_file, scat_file, impulse_file,  res=40, t_window=20):
-    k0_field_dict={'location':['latitude','longitude'], 
+    k0_field_dict={'location':['latitude','longitude'],
             'both':['K0']}
     catalog=setup(scat_file, impulse_file)
     D_in=pc.data.from_file(k0_file, field_dict=k0_field_dict)
@@ -122,18 +124,21 @@ def make_composite_stats(k0_file, scat_file, impulse_file,  res=40, t_window=20)
     D_out=pc.data().from_dict(
         {field:np.zeros(len(pt_dict))+np.NaN for field in \
          ['t_ctr', 't_med','t_sigma', 't_sigma_r','N','x','y', 'z_sigma', 'z_slope_mag']})
-    
+
     for ii, ctr in enumerate(pt_dict):
         N=len(pt_dict[ctr])
         if N < 10:
             continue
-        WF0 = make_composite_wf(D_in.k0[pt_dict[ctr]], catalog)[0]        
+        WF0 = make_composite_wf(D_in.k0[pt_dict[ctr]], catalog)[0]
         D_out.t_ctr[ii], D_out.t_med[ii], D_out.t_sigma[ii], D_out.t_sigma_r[ii]=composite_stats(WF0)
         D_out.sigma_z[ii], D_out.z_slope_mag[ii] = plane_fit_R(D_in, pt_dict[ctr])
-    
+
 
 def make_plot(k0_file, scat_file, impulse_file,  xy0=None, res=40, t_window=20):
-    k0_field_dict={'location':['latitude','longitude'], 
+
+    res_test(impulse_file)
+
+    k0_field_dict={'location':['latitude','longitude'],
             'both':['K0']}
     catalog=setup(scat_file, impulse_file)
     D_in=pc.data().from_h5(k0_file, field_dict=k0_field_dict)
@@ -142,19 +147,18 @@ def make_plot(k0_file, scat_file, impulse_file,  xy0=None, res=40, t_window=20):
     xy_fits=np.concatenate([np.array(key).reshape([1,2]) for key in pt_dict], axis=0)
     best=np.argmin(np.abs((xy_fits[:,0]-xy0[0])+1j*(xy_fits[:,1]-xy0[1])))
     WF_composite, WFs=make_composite_wf(D_in.K0[pt_dict[tuple(xy_fits[best,:])]], catalog)
-    
+
     TX=catalog[0]
 
     bar0, med0, sigma0, sigmar_0=composite_stats(TX, t_window)
     barc, medc, sigmac, sigmar_c=composite_stats(WF_composite, t_window)
-    
+
     TX_corr_mean_c, TX_corr_med_c=TX_corr(TX, np.max([6*sigmar_c, t_window]), sigmar_c, TX_sigma=sigma0)
     TX_corr_mean_0, TX_corr_med_0=TX_corr(TX, np.max([6*sigmar_0, t_window]), sigmar_0, TX_sigma=sigma0)
 
-    
     plt.figure()
     z0 = TX.centroid()*-0.15
-    
+
     for WF in WFs:
         plt.plot(WF.p, WF.t*-.15-z0, linewidth=0.5, color='gray')
     plt.plot(catalog[0].p, catalog[0].t*-.15-z0, 'k', linewidth=2)
@@ -163,9 +167,29 @@ def make_plot(k0_file, scat_file, impulse_file,  xy0=None, res=40, t_window=20):
     plt.gca().axhline(-barc*.15-z0, color='b', linewidth=2)
     plt.gca().axhline(-med0*.15-z0, linestyle='--', color='k', linewidth=2)
     plt.gca().axhline(-medc*.15-z0, linestyle='--', color='b', linewidth=2)
-    
+
     plt.xlabel('power')
     plt.ylabel('elevation WRT surface, m')
+
+def res_test(impulse_file):
+
+    with h5py.File(impulse_file,'r') as h5f:
+        TX=waveform(np.array(h5f['/TX/t'])*1.e9,np.array(h5f['/TX/p']))
+    dt_vals=[0.25, 0.1, 0.05, 0.025, 0.0125, 0.0125/2, 0.0125/4]
+    ctr_vals=np.zeros_like(dt_vals)+np.NaN
+    med_vals=np.zeros_like(dt_vals)+np.NaN
+    wctr_vals=np.zeros_like(dt_vals)+np.NaN
+    wmed_vals=np.zeros_like(dt_vals)+np.NaN
+    for ii, dt in enumerate(dt_vals):
+        ti=np.arange(TX.t[0], TX.t[-1]+dt, dt)
+        ti=ti.reshape([ti.size, 1])
+        TXi=waveform(ti, np.interp(ti, TX.t.ravel(), TX.p.ravel()).reshape(ti.shape))
+        ctr_vals[ii], med_vals[ii]=wf_med_bar(TXi, 20., t_tol=0.001)
+
+    plt.figure(11); plt.clf()
+    plt.semilogx(dt_vals, ctr_vals)
+    plt.semilogx(dt_vals, med_vals)
+
 
 def main():
     parser=ArgumentParser("add simulated waveforms based on k0 values in files, report IECsat-2 time delays")
@@ -177,17 +201,16 @@ def main():
     parser.add_argument('--t_window','-t', type=float, default=20, help='time window for elevation calculations, default of 20 ns matches ATL06')
     parser.add_argument('--xy', type=float, nargs=2, help='make plot for location x, y')
     args=parser.parse_args()
-    
+
     if args.xy is not None:
         make_plot(args.fit_file, args.scat_file, args.impulse_file, xy0=args.xy,  res=args.width, t_window=args.t_window)
         return
     D_out=make_composite_stats(args.fit_file, args.scat_file, args.impulse_file, res=args.width, t_window=args.t_window)
     D_out.to_h5(args.out_file)
-    
+
 if __name__=='__main__':
     main()
-    
-    
+
+
 # note: these input arguments:
-    #/Volumes/ice2/ben/ATM_WF/AA_18/fits/2018.11.10/ILNSAW1B_20181110_181100.atm6DT7.h5_q4_out.h5 -s SRF_green_full.h5 -i TEP.h5 -w 40 --xy -511599.5 302234.5
-    
+    #ILNSAW1B_20181110_181100.atm6DT7.h5_q4_out.h5 -s SRF_green_full.h5 -i TEP.h5 -w 40 --xy -511599.5 302234.5
