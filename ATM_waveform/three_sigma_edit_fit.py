2e26c5c5787b24b92c8b6ac0292970a801d910f9
Ben Smith <besmith@uw.edu>
2021-02-15 14:28:33 -0500

    ATM_waveform/three_sigma_edit_fit.py: better exception handling

--- ATM_waveform/three_sigma_edit_fit.py
+++ ATM_waveform/three_sigma_edit_fit.py
@@ -19,20 +19,27 @@ def RDE(x):
     return (LH[1]-LH[0])/2.  # trying to get some kind of a width of the data ~variance
 
 
-def three_sigma_edit_fit(G0, d0, n_iterations=5):
-    good0=np.all(np.isfinite(G0) & np.isfinite(d0))
-    good=good0
+def three_sigma_edit_fit(G0, d0, n_iterations=5, sigma_min=0):
+    good0=np.all(np.isfinite(G0), axis=1) & np.isfinite(d0)
+    good=good0.copy()
     m=np.zeros(G0.shape[1])
     r=np.zeros(d0.shape)
     sigma=np.NaN
-    good=np.zeros_like(d0, dtype=bool)
-    good_last=good.copy()
+    good_last=np.zeros_like(d0, dtype=bool)
     for ii in range(n_iterations+1):
         G=G0[good,:]
-        if (G.shape[1]<G.shape[0]) or np.all(good_last==good):
+        if (G.shape[0]<G.shape[1]) or np.all(good_last==good):
             break
-        m=np.linalg.solve(G.T.dot(G), G.T.dot(d0[good]))
-        r=d0-G0.dot(m)
+        try:
+            m=np.linalg.solve(G.T.dot(G), G.T.dot(d0[good]))
+        except Exception as E:
+            m=np.zeros(G.shape[1])+np.NaN
+            r=np.zeros_like(d0)+np.NaN
+            sigma=np.NaN
+            good=np.zeros_like(d0, dtype=bool)
+            return m, r, sigma, good
+        r=d0[good0]-G0[good0,:].dot(m)
         sigma=RDE(r[good])
-        good=good0 & (np.abs(r)<3*sigma)
+        good = good0 & ( np.abs(r) < 3*np.maximum(sigma, sigma_min) )
+        
     return m, r, sigma, good
