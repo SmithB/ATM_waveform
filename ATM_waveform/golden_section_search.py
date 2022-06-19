95d5d7afce054ecf4825b328e0ad0cce891136d6
smithB <bsmith@apl.washington.edu>
2022-06-18 20:51:20 -0700

    ATM_waveform/golden_section_search.py: avoid searching for minimum

--- ATM_waveform/golden_section_search.py
+++ ATM_waveform/golden_section_search.py
@@ -5,7 +5,7 @@ Created on Tue Jun  14 20:41:00 2022
 @author: ben
 """
 import numpy as np
-
+from sortedcontainers.sortedset import SortedSet
 
 def parabolic_search_refinement(x, R):
     """
@@ -14,6 +14,7 @@ def parabolic_search_refinement(x, R):
     ind=np.argsort(x)
     xs=x[ind]
     Rs=R[ind]
+
     best=np.argmin(R)
     if best==0:
         p_ind=[0, 1, 2]
@@ -53,18 +54,24 @@ def golden_section_search(f, x0, delta_x, bnds=[-np.Inf, np.Inf], \
         phi=2
     b=1.-1./phi
     a=1./phi
-    # if x0 is not a list or an array, wrap it in a list so we can iterate
-    if not hasattr(x0,'__iter__'):
-        x0=[x0]
-    searched=np.array([])
+
+    searched=SortedSet()
     R_dict=dict()
     it_count=0
     largest_gap=np.Inf
+    if step_size is not None and hasattr(step_size, '__iter__'):
+        step_size=step_size[0]
+
     while (len(searched)==0) or (largest_gap > tol):
-        for x in x0:
-            # Calculate the residuals here:
-            R_dict[x]=f(x, *fn_args, **fn_kwargs)
-            searched=np.union1d(searched, [x])
+        if hasattr(x0, '__iter__'):
+            for x in x0:
+                # Calculate the residuals here:
+                R_dict[x]=f(x, *fn_args, **fn_kwargs)
+                #searched=np.union1d(searched, [x])
+            searched.update(x0)
+        else:
+            R_dict[x0]=f(x0, *fn_args, **fn_kwargs)
+            searched.add(x0)
         # make a list of R_vals searched
         R_vals=np.array([R_dict[x] for x in searched])
         # find the minimum of the R vals
@@ -75,18 +82,26 @@ def golden_section_search(f, x0, delta_x, bnds=[-np.Inf, np.Inf], \
         # choose the next search point.  If the search picked the minimum or maximum of the
         # time offsets, take a step  of delta_x to the left or right
         if iR==0:
-            x0 = [searched[0] - delta_x]
-            if x0[0] < bnds[0]:
+            x0 = searched[0] - delta_x
+            if x0 < bnds[0]:
                 # if we're out of bounds, search between the minimum searched value and the left boundary
-                x0=[a*bnds[0]+b*np.min(searched[searched > bnds[0]])]
-                largest_gap=searched[1]-searched[0]
+                if x0 in searched:
+                    ep = [ bnds[0], searched[1] ]
+                else:
+                    ep = [ bnds[0], searched[0] ]
+                x0 = a*ep[0] + b*ep[1]
+                largest_gap = ep[1]-ep[0]
             else:
                 largest_gap=np.Inf
         elif iR==len(searched)-1:
-            x0 = [searched[-1] + delta_x ]
-            if x0[0] > bnds[1]:
-                x0=[a*bnds[1]+b*np.max(searched[searched < bnds[1]])]
-                largest_gap=searched[-1]-searched[-2]
+            x0 = searched[-1] + delta_x
+            if x0 > bnds[1]:
+                if bnds[1] in searched:
+                    ep = [bnds[1], searched[-2]]
+                else:
+                    ep = [bnds[1], searched[-1]]
+                x0=a*ep[0] +b*ep[1]
+                largest_gap = ep[0]-ep[1]
             else:
                 largest_gap=np.Inf
         else:
@@ -94,18 +109,18 @@ def golden_section_search(f, x0, delta_x, bnds=[-np.Inf, np.Inf], \
             # around the minimum, and add a point using a golden-rule search
             if searched[iR+1]-searched[iR] > searched[iR]-searched[iR-1]:
                 # the gap to the right of the minimum is largest: put the new point there
-                x0 = [ a*searched[iR] + b*searched[iR+1] ]
+                x0 = a*searched[iR] + b*searched[iR+1]
                 largest_gap=searched[iR+1]-searched[iR]
             else:
                 # the gap to the left of the minimum is largest: put the new point there
-                x0 = [ a*searched[iR] + b*searched[iR-1] ]
+                x0 = a*searched[iR] + b*searched[iR-1]
                 largest_gap=searched[iR]-searched[iR-1]
         if integer_steps is True:
             # convert x0 to an integer step if it isn't already
-            if np.floor(np.array(x0)/step_size).astype(int)*step_size not in searched:
-                x0=np.floor(np.array(x0)/step_size).astype(int)*step_size
-            elif np.ceil(np.array(x0)/step_size).astype(int)*step_size not in searched:
-                x0=np.ceil(np.array(x0)/step_size).astype(int)*step_size
+            if int(np.floor(x0/step_size))*step_size not in searched:
+                x0=int(np.floor(x0/step_size))*step_size
+            elif int(np.ceil(x0/step_size))*step_size not in searched:
+                x0=int(np.ceil(x0/step_size))*step_size
             else:
                 # exit if we have searched both the integer below and the integer above x0
                 break
@@ -114,6 +129,7 @@ def golden_section_search(f, x0, delta_x, bnds=[-np.Inf, np.Inf], \
         if it_count > max_count:
             print("WARNING: too many shifts")
             break
+    searched=np.array(searched)
     search_hist.update({'x':searched, 'R':R_vals})
     if refine_parabolic and (len(searched) >= 3):
         x_ref, R_ref = parabolic_search_refinement(searched, R_vals)
@@ -123,3 +139,4 @@ def golden_section_search(f, x0, delta_x, bnds=[-np.Inf, np.Inf], \
             return x_ref, R_ref
     else:
         return searched[iR], R_vals[iR]
+
